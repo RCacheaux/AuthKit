15b2a54e640a1a38d1d11e35a1949a31cf9104de
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2013-03-23 17:18:27 -0500

    Project setup for accounts lib.

---
+++ AuthKit/AuthKitTests/Accounts/AKAccountsTests.m
@@ -0,0 +1,13 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AKAccountsTests : SenTestCase
+
+@end
+
+@implementation AKAccountsTests
+
+- (void)testUnitTest {
+  STAssertEquals(1, 2, nil);
+}
+
+@end
